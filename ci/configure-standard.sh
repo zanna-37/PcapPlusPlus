4d2e88b2523c952b0271d76eeab89d7ed0f67c76
seladb <seladb@yahoo.com>
2019-05-23 05:06:27 -0400

    Another small fix in CI scripts

--- ci/configure-standard.sh
+++ ci/configure-standard.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Usage:
-#    configure for MacOS            --> ./configure-standard.sh macos
+#    configure for MacOS            --> ./configure-standard.sh osx
 #    configure for Linux            --> ./configure-standard.sh linux
 #    configure with dpdk (Linux)    --> ./configure-standard.sh linux dpdk <DPDK_HOME>
 #    configure with pf_ring (Linux) --> ./configure-standard.sh linux pf_ring <PF_RING_HOME>
@@ -23,7 +23,7 @@ if [[ "$2" == "pf_ring" ]]; then
     PF_RING_HOME=$3;
 fi
 
-if [[ "$PLATFORM_OS" == "macos" ]]; then ./configure-mac_os_x.sh; fi
+if [[ "$PLATFORM_OS" == "osx" ]]; then ./configure-mac_os_x.sh; fi
 if [[ "$PLATFORM_OS" == "linux" ]]; then ./configure-linux.sh --default; fi;
 if [[ $COMPILE_WITH_DPDK > 0 ]]; then ./configure-linux.sh --dpdk --dpdk-home $DPDK_HOME; fi;
 if [[ $COMPILE_WITH_PF_RING > 0 ]]; then ./configure-linux.sh --pf-ring --pf-ring-home $PF_RING_HOME; fi;
