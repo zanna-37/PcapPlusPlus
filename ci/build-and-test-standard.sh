1167f0420eb5ae6241fe76e10d0197f00e8a7a36
seladb <seladb@yahoo.com>
2019-05-23 04:58:34 -0400

    Another small fix

--- ci/build-and-test-standard.sh
+++ ci/build-and-test-standard.sh
@@ -3,7 +3,7 @@ set -e
 
 make all
 cd Tests/Packet++Test && Bin/Packet++Test
-cd ../Pcap++Test && Bin/Pcap++Test -n
+cd ../Pcap++Test && Bin/Pcap++Test -n -k 0
 cd ../../ && make install
 cd Examples/Tutorials/Tutorial-HelloWorld && make -f Makefile.non_windows all;
 ./Tutorial-HelloWorld;
